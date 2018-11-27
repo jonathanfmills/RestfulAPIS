f22fe79df2d0f1edba3d2128e1a1b0c8e66dfb2f
Jon Mills <jonathanfmills@gmail.com>
2018-11-26 20:23:04 -0600

    Wiring up to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPITest');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'This is my API' };
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
 app.use('/api', bookRouter);
 
