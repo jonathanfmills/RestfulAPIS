d4b7d4f02d9aa20d98817406c20fb000569f0dfd
Jon Mills <jonathanfmills@gmail.com>
2018-12-02 15:06:06 -0600

    Filtering with a query string

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find((err, books) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
       if (err) {
         return res.send(err);
       }
