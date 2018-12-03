0be30a7af6246bbeb497b4a49bfae9eba69ee0a2
Jon Mills <jonathanfmills@gmail.com>
2018-12-02 21:00:18 -0600

    Injecting the book model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPITest');
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
-const bookRouter = require('./routes/bookRouter');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-function routes() {
+function routes(Book) {
   const bookRouter = express.Router();
   bookRouter.route('/books')
     .post((req, res) => {
