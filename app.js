c3ddf579d8e0c10195ab35a8cf6b742b566c6b03
Jon Mills <jonathanfmills@gmail.com>
2018-12-02 20:37:59 -0600

    Code cleanup

--- app.js
+++ app.js
@@ -4,41 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPITest');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
