feff02dee6b11f870c56a26f0e73be65cdc11eb9
Jon Mills <jonathanfmills@gmail.com>
2018-12-08 17:25:55 -0600

    Implementing remove

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -71,8 +71,15 @@ function routes(Book) {
         }
         return res.json(book);
       });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
     });
-
   return bookRouter;
 }
 
