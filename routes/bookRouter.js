579b60614d85fd8858d4bbb8a6481bf3d64d6610
Jon Mills <jonathanfmills@gmail.com>
2018-12-08 17:20:55 -0600

    Implementing Patch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -7,8 +7,12 @@ function routes(Book) {
     .post((req, res) => {
       const book = new Book(req.body);
 
-      book.save();
-      return res.status(201).json(book);
+      book.save((err, addedBook) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.status(201).json(addedBook);
+      });
     })
     .get((req, res) => {
       const query = {};
@@ -42,6 +46,25 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
       book.save((err) => {
         if (err) {
           return res.send(err);
