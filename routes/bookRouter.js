fe1d9377c1be3dd97887cdf4f2ae15ed3c2c5654
Jon Mills <jonathanfmills@gmail.com>
2018-12-08 17:02:21 -0600

    Middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -22,25 +22,30 @@ function routes(Book) {
         return res.json(books);
       });
     });
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
-    })
+    .get((req, res) => res.json(req.book))
     .put((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save((err) => {
         if (err) {
           return res.send(err);
         }
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.author;
-        book.read = req.body.read;
-        book.save();
         return res.json(book);
       });
     });
