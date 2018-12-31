4e581b237f43cf4b8c429f88d2a0f450c8a618f5
Jon Mills <jonathanfmills@gmail.com>
2018-12-30 19:27:37 -0600

    Controllers

---
+++ controllers/booksController.js
@@ -0,0 +1,31 @@
+const Book = require('../models/bookModel');
+
+function bookController() {
+  function post(req, res) {
+    const book = new Book(req.body);
+
+    book.save((err, addedBook) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.status(201);
+      return res.json(addedBook);
+    });
+  }
+
+  function get(req, res) {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  }
+  return { post, get };
+}
+
+module.exports = bookController();
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,31 +1,14 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
+const booksController = require('../controllers/booksController');
 
 function routes(Book) {
   const bookRouter = express.Router();
-  bookRouter.route('/books')
-    .post((req, res) => {
-      const book = new Book(req.body);
+  const controller = booksController;
 
-      book.save((err, addedBook) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.status(201).json(addedBook);
-      });
-    })
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-    });
+  bookRouter.route('/books')
+    .post(controller.post)
+    .get(controller.get);
   bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
